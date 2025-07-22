ba645932e537254124d60edc92420e57060e24d4
hungrystartup <redbarcompany+render@gmail.com>
2025-07-21 17:26:12 -0700

    Update redis.js

--- server/redis.js
+++ server/redis.js
@@ -4,20 +4,22 @@ require('dotenv').config();
 const redisClient = createClient({
     url: process.env.REDIS_URL,
     socket: {
-        tls: true, // Required for Upstash
+        tls: true,
         reconnectStrategy: retries => Math.min(retries * 50, 2000)
     }
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
-(async () => {
-    try {
-        await redisClient.connect();
-        console.log("Connected to Redis");
-    } catch (err) {
-        console.error("Failed to connect to Redis:", err);
+const connectRedis = async () => {
+    if (!redisClient.isOpen) {
+        try {
+            await redisClient.connect();
+            console.log("Connected to Redis");
+        } catch (err) {
+            console.error("Failed to connect to Redis:", err);
+        }
     }
-})();
+};
 
-module.exports = redisClient;
+module.exports = { redisClient, connectRedis };
