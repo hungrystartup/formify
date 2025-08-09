1cc96c5320c7a67086c78e1ee1566c5961d1453e
hungrystartup <redbarcompany+render@gmail.com>
2025-08-08 23:08:38 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials: true,
+    credentials: false,
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
