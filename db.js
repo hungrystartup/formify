fa2cc9c043d07e0946fb13c164188a48cf6d6fc9
Solomon Olatunji <realsolomon@outlook.com>
2025-08-09 07:55:32 +0100

    Refactor db.js and server.js for improved readability; add Prettier for code formatting in package.json and package-lock.json

--- db.js
+++ db.js
@@ -2,15 +2,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const conn = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
 
 conn.connect(err => {
-    if(err) throw err;
-    console.log("Connected to database");
+  if (err) throw err;
+  console.log('Connected to database');
 });
 
 module.export = conn;
--- server/redis.js
+++ server/redis.js
@@ -2,24 +2,24 @@ const { createClient } = require('redis');
 require('dotenv').config();
 
 const redisClient = createClient({
-    url: process.env.REDIS_URL,
-    socket: {
-        tls: true,
-        reconnectStrategy: retries => Math.min(retries * 50, 2000)
-    }
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    reconnectStrategy: retries => Math.min(retries * 50, 2000),
+  },
 });
 
-redisClient.on('error', (err) => console.error('Redis Client Error', err));
+redisClient.on('error', err => console.error('Redis Client Error', err));
 
 const connectRedis = async () => {
-    if (!redisClient.isOpen) {
-        try {
-            await redisClient.connect();
-            console.log("Connected to Redis");
-        } catch (err) {
-            console.error("Failed to connect to Redis:", err);
-        }
+  if (!redisClient.isOpen) {
+    try {
+      await redisClient.connect();
+      console.log('Connected to Redis');
+    } catch (err) {
+      console.error('Failed to connect to Redis:', err);
     }
+  }
 };
 
 module.exports = { redisClient, connectRedis };
